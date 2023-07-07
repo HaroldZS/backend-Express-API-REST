0bba83c4f42d6303069fa4b653f89fb256386fc1
haroldZS <haroldzs1024@gmail.com>
2023-07-07 13:03:09 -0400

    Globla error middleware

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErrors, errorHandler } = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3005;
 
@@ -15,6 +17,9 @@ routerApi(app);
 app.use((req, res) => {
   res.status(404).send('Route not found...');
 });
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Listening in port: ${port}`);
