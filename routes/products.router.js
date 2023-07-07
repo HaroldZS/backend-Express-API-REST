6714f95c2c98ddda57cecbab9fecf1cc2f0a50e4
haroldZS <haroldzs1024@gmail.com>
2023-07-07 18:21:19 -0400

    Validator middleware

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const ProductService = require('./../services/product.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+} = require('./../schemas/product.schema');
 
 const router = express.Router();
 const productService = new ProductService();
@@ -13,32 +19,45 @@ router.get('/filter', (req, res) => {
   res.send('Filter');
 });
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const product = await productService.findOne(id);
-    res.json(product);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await productService.findOne(id);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
-router.post('/', async (req, res) => {
-  const body = req.body;
-  const newProduct = await productService.create(body);
-  res.status(201).json(newProduct);
-});
-
-router.patch('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
+router.post(
+  '/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
     const body = req.body;
-    const product = await productService.partialUpdate(id, body);
-    res.status(200).json(product);
-  } catch (error) {
-    next(error);
-  }
-});
+    const newProduct = await productService.create(body);
+    res.status(201).json(newProduct);
+  },
+);
+
+router.patch(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = await productService.partialUpdate(id, body);
+      res.status(200).json(product);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
