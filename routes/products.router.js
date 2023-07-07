7eb31046f7cbb8a99cb556a4f8683f4530494256
haroldZS <haroldzs1024@gmail.com>
2023-07-06 23:49:15 -0400

    Services

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,20 +1,11 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const ProductService = require('./../services/product.service');
 
 const router = express.Router();
+const productService = new ProductService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-
+  const products = productService.find();
   res.json(products);
 });
 
@@ -24,17 +15,8 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  if (id === '999') {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  } else {
-    res.status(200).json({
-      id,
-      name: 'Product 1',
-      price: 1000,
-    });
-  }
+  const product = productService.findOne(id);
+  res.json(product);
 });
 
 router.post('/', (req, res) => {
