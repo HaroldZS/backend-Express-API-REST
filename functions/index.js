2e5664db3721ec036feb78b62a2725a33b79a62c
haroldZS <haroldzs1024@gmail.com>
2023-07-08 00:41:16 -0400

    Demo deploy

---
+++ functions/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const cors = require('cors');
+const routerApi = require('../routes');
+
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('../middlewares/error.handler');
+
+const app = express();
+// const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// const whitelist = ['http://127.0.0.1:5500', 'https://myapp.com'];
+// const options = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Permission denied'));
+//     }
+//   },
+// };
+
+// app.use(cors(options));
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hi there, this is my server with express!');
+});
+
+routerApi(app);
+
+app.use((req, res) => {
+  res.status(404).send('Route not found...');
+});
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
+
+// app.listen(port, () => {
+//   console.log(`Listening in port: ${port}`);
+// });
